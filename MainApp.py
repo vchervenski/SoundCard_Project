cbb11d478831c7d305824bd0aa2e971febb16992
Valeri Chervenski <val.chervenski@gmail.com>
2018-11-12 23:50:00 +0200

    Update MainApp.py

--- MainApp.py
+++ MainApp.py
@@ -3,13 +3,11 @@ from MainAppController import MainAppController
 
 
 def main():
-    controller = MainAppController()
-
     # Build Gui and start it
     root = tk.Tk()
     root.title('Main Application')
     root.geometry("500x300")
-    controller.init_view(root)
+    controller = MainAppController(root)
 
 
 if __name__ == "__main__":
